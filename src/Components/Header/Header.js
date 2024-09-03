3add82e00d316ece916b5176a8b6f9606e1f2673
Vineesh <vineesh.web.solwyz@gmail.com>
2024-09-03 14:13:47 +0530

    navBar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,42 +1,87 @@
-import React from 'react'
-import MainLogo from "../../Assets/mainLogo.png"
-import Dot from "../../Assets/Icon/Ellipse 1.svg"
-import "./Header.css"
-import ButtonArrow from "../../Assets/Icon/arrow_outward.svg"
+import React, { useState } from 'react';
+import MainLogo from "../../Assets/mainLogo.png";
+import Dot from "../../Assets/Icon/Ellipse 1.svg";
+import ButtonArrow from "../../Assets/Icon/arrow_outward.svg";
+import "./Header.css";
 
 function Header() {
+  const [activeLink, setActiveLink] = useState('Home'); // Set 'Home' as the default active link
+
+  const handleClick = (link) => {
+    setActiveLink(link);
+  };
+
   return (
     <div>
-   <div className='sm:w-[1354px] flex'>
-   <div className='logoSection'>
- <img className='sm:w-[58px] sm:h-[66px] ' src={MainLogo} alt="" />
-   </div>
-   <div className='NavSection sm:w-[698px] columns-auto flex gap-4 sm:text-[16px] sm:leading-5 sm:font-light my-auto'>
-    <div>Home</div>
-    <img className='sm:w-[8px] sm:h-[8px] my-auto' src={Dot} alt="" />
-    <div>About Us</div>
-    <img className='sm:w-[8px] sm:h-[8px] my-auto' src={Dot} alt="" />
-    <div>Services</div>
-    <img className='sm:w-[8px] sm:h-[8px] my-auto' src={Dot} alt="" />
-    <div>Products</div>
-    <img className='sm:w-[8px] sm:h-[8px] my-auto' src={Dot} alt="" />
-    <div>Blog</div>
-    <img className='sm:w-[8px] sm:h-[8px] my-auto' src={Dot} alt="" />
-    <div>Contact Us</div>
-    <img className='sm:w-[8px] sm:h-[8px] my-auto' src={Dot} alt="" />
-    <div>Career</div>
-    <img className='sm:w-[8px] sm:h-[8px] my-auto' src={Dot} alt="" />
-    <div>Gallery</div>
-  </div>
+      <div className='sm:w-[1354px] flex sm:ml-[54px] sm:pt-4 '>
+        <div className='logoSection'>
+          <img className='sm:w-[58px] sm:h-[66px]' src={MainLogo} alt="Main Logo" />
+        </div>
+        <div className='NavSection sm:w-[698px] columns-auto flex gap-4 sm:text-[16px] sm:leading-5 sm:font-light my-auto sm:ml-[295px]'>
+          <div
+            className={`nav-item ${activeLink === 'Home' ? 'font-semibold' : ''}`}
+            onClick={() => handleClick('Home')}
+          >
+            Home
+          </div>
+          <img className='sm:w-[8px] sm:h-[8px] my-auto' src={Dot} alt="" />
+          <div
+            className={`nav-item ${activeLink === 'About Us' ? 'font-semibold' : ''}`}
+            onClick={() => handleClick('About Us')}
+          >
+            About Us
+          </div>
+          <img className='sm:w-[8px] sm:h-[8px] my-auto' src={Dot} alt="" />
+          <div
+            className={`nav-item ${activeLink === 'Services' ? 'font-semibold' : ''}`}
+            onClick={() => handleClick('Services')}
+          >
+            Services
+          </div>
+          <img className='sm:w-[8px] sm:h-[8px] my-auto' src={Dot} alt="" />
+          <div
+            className={`nav-item ${activeLink === 'Products' ? 'font-semibold' : ''}`}
+            onClick={() => handleClick('Products')}
+          >
+            Products
+          </div>
+          <img className='sm:w-[8px] sm:h-[8px] my-auto' src={Dot} alt="" />
+          <div
+            className={`nav-item ${activeLink === 'Blog' ? 'font-semibold' : ''}`}
+            onClick={() => handleClick('Blog')}
+          >
+            Blog
+          </div>
+          <img className='sm:w-[8px] sm:h-[8px] my-auto' src={Dot} alt="" />
+          <div
+            className={`nav-item ${activeLink === 'Contact Us' ? 'font-semibold' : ''}`}
+            onClick={() => handleClick('Contact Us')}
+          >
+            Contact Us
+          </div>
+          <img className='sm:w-[8px] sm:h-[8px] my-auto' src={Dot} alt="" />
+          <div
+            className={`nav-item ${activeLink === 'Career' ? 'font-semibold' : ''}`}
+            onClick={() => handleClick('Career')}
+          >
+            Career
+          </div>
+          <img className='sm:w-[8px] sm:h-[8px] my-auto' src={Dot} alt="" />
+          <div
+            className={`nav-item ${activeLink === 'Gallery' ? 'font-semibold' : ''}`}
+            onClick={() => handleClick('Gallery')}
+          >
+            Gallery
+          </div>
+        </div>
 
-<button className='GetInTouchBtn flex '>
-    <span>Get In Touch</span>
-   <img src={ButtonArrow} alt="" />
-</button>
-
-   </div>
+        <button className='GetInTouchBtn flex sm:ml-[90px]'>
+          <span className='text-white'>Get In Touch</span>
+          <img className='sm:w-6 sm:h-6' src={ButtonArrow} alt="Button Arrow" />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import AboutUsHome from '../../Components/HomeComponents/AboutUsHome/AboutUsHome'
+import BannerComponent from '../../Components/HomeComponents/BannerComponent/BannerComponent'
 
 function HomePage() {
   return (
     <div>
+      <BannerComponent/>
       <AboutUsHome/>
     </div>
   )
